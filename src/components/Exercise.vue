365756e8d53e92cc1729769bd9aa910c77c45c34
Hsu Wai Wai Hnin <hsuwai2hnin@gmail.com>
2020-09-29 19:06:15 +0630

    data using from API

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -1,13 +1,42 @@
 <template>
-	<h1 class="text-danger">This is an Exercise Component!</h1>
-	
+  <div class="exercise">
+    <h1 class="text-success mt-3">This All Tags!</h1>
+
+    <div class="row">
+      <div class="col-md-3 my-3" v-for="(tag,index) in tags" :key="index">
+        <b-card :header="tag.tag_name" class="text-left" >
+        <b-card-text class="text-success">{{tag.tag_description.substring(0,30)+"..."}}</b-card-text>
+      </b-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/javascript">
-	
+  import MyService from '@/services/MyServices.js'
+  export default{
+    data(){
+      return{
+        tags:[]
+      }
+    },
+    mounted(){
+      this.getAllTags();
+    },
+    methods:{
+      getAllTags(){
+        MyService.getTags()
+                .then(res => {
+                  this.tags = res.data.tags
+                })
+                .catch(err=>{
+                  console.error(err)
+                })
+      }
+    }
+  }
 </script>
 
-
 <style type="text/css">
-	
+  
 </style>
--- src/components/Testing.vue
+++ src/components/Testing.vue
@@ -1,11 +1,44 @@
 <template>
 	<div class="testing">
-		<h1>Hello Testing Vue</h1>
-	</div>
+    <h1 class="text-danger mt-3">This All Posts!</h1>
+    
+    <div class="row">
+      <div class="col-md-8 my-3 text-left" v-for="(row,index) in posts" :key="index">
+        <h4>{{row.post_title}}</h4>
+        <p>{{row.post_content}}</p>
+
+        <button class="btn-sm btn-dark mr-3" v-for="(tag,index) in row.post_tags" :key="index">{{tag.tag_name}}</button>
+
+        <!-- <button class="btn-sm btn-dark mx-3">{{row.post_category}}</button> -->
+      </div>
+    </div>
+  </div>
 	
 </template>
 
 <script type="text/javascript">
+	import MyService from '@/services/MyServices.js'
+  export default{
+    data(){
+      return{
+        posts:[]
+      }
+    },
+    mounted(){ // auto working
+      this.getAllPosts()
+    },
+    methods:{
+      getAllPosts(){
+        MyService.getPosts()
+                .then(res => {
+                  this.posts = res.data.posts
+                })
+                .catch(err=>{
+                  console.error(err)
+                })
+      }
+    }
+  }
 	
 </script>
 
