69d2120e980002d59bbd6d703a858e771a4a063c
Hsu Wai Wai Hnin <hsuwai2hnin@gmail.com>
2020-09-29 15:28:18 +0630

    first commit

---
+++ src/components/Exercise.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1 class="text-danger">This is an Exercise Component!</h1>
+	
+</template>
+
+<script type="text/javascript">
+	
+</script>
+
+
+<style type="text/css">
+	
+</style>
---
+++ src/components/Testing.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="testing">
+		<h1>Hello Testing Vue</h1>
+	</div>
+	
+</template>
+
+<script type="text/javascript">
+	
+</script>
+
+<style type="text/css">
+	
+</style>
